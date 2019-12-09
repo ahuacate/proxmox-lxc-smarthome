477f00d659b796deae32ed9c0217f22d0fc70e91
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2019-12-09 11:39:49 +0700

    Update nodered_setup.sh

--- scripts/nodered_setup.sh
+++ scripts/nodered_setup.sh
@@ -21,46 +21,20 @@ function msg() {
 mkdir -p /home/typhoon/nodered
 chown 1607:65607 -R /home/typhoon/nodered
 
-# Install prerequisites
-msg "Installing Node-Red prerequisites..."
-apt-get install -y npm
-
 # Install Node-Red
 msg "Installing Node-Red..."
-npm install -g --unsafe-perm node-red
-
-# Create Node-Red Service
-msg "Creating nodered service..."
-cat << EOF > /etc/systemd/system/node-red.service
-[Unit]
-Description=node-red graphical event wiring tool
-Wants=network.target
-Documentation=http://nodered.org
-
-[Service]
-Type=simple
-User=typhoon
-Group=privatelab
-WorkingDirectory=/home/typhoon/nodered
-Nice=5
-#Environment="NODE_OPTIONS=--max_old_space_size=256"
-# uncomment the next line for a more verbose log output
-#Environment="NODE_RED_OPTIONS=-v"
-ExecStart=/usr/bin/env node-red $NODE_OPTIONS $NODE_RED_OPTIONS
-KillSignal=SIGINT
-Restart=on-failure
-SyslogIdentifier=node-red
-
-[Install]
-WantedBy=multi-user.target
-EOF
+apt-get update  >/dev/null
+apt-get install -y curl  >/dev/null
+echo
+echo -e "You will now be prompted with (y/N) inputs. \nAnswer 'Y' to all prompts."
+echo
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) - typhoon
 
 # Start Node-Red Service
 msg "Starting Node-Red service..."
 systemctl daemon-reload
-systemctl enable node-red.service
-systemctl start node-red.service
-systemctl status node-red.service
+systemctl enable nodered.service
+systemctl start nodered.service
 
 # Cleanup container
 msg "Cleanup..."
